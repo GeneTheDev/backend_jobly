74142c18f70d9b4841a0cffa06465b5c1654cdd9
gene21gene <owens.eugene@yahoo.com>
2023-05-22 21:57:35 -0400

    updating path module

--- app.js
+++ app.js
@@ -18,10 +18,10 @@ const morgan = require("morgan");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "../build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/../build/index.html"));
+  res.sendFile(path.join(__dirname + "../build/index.html"));
 });
 
 app.use(cors());
