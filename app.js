de937c2b4ab664f6c5a20045d596181c8e98d14e
gene21gene <owens.eugene@yahoo.com>
2023-05-23 22:34:58 -0400

    updating app.js

--- app.js
+++ app.js
@@ -23,16 +23,10 @@ app.use(express.json());
 app.use(morgan("tiny"));
 app.use(authenticateJWT);
 
-app.use(express.static(path.join(__dirname, "./build")));
-
 app.use("/auth", authRoutes);
 app.use("/companies", companiesRoutes);
 app.use("/users", usersRoutes);
 app.use("/jobs", jobsRoutes);
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./build/index.html"));
-});
 
 /** Handle 404 errors -- this matches everything */
 app.use(function (req, res, next) {
