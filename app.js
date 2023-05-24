4185d72035501e16536dcf1cc0719623b684d13f
gene21gene <owens.eugene@yahoo.com>
2023-05-23 22:28:15 -0400

    updating app.js

--- app.js
+++ app.js
@@ -25,15 +25,14 @@ app.use(authenticateJWT);
 
 app.use(express.static(path.join(__dirname, "../build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../build/index.html"));
-});
-
 app.use("/auth", authRoutes);
 app.use("/companies", companiesRoutes);
 app.use("/users", usersRoutes);
 app.use("/jobs", jobsRoutes);
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build/index.html"));
+});
 /** Handle 404 errors -- this matches everything */
 app.use(function (req, res, next) {
   return next(new NotFoundError());
